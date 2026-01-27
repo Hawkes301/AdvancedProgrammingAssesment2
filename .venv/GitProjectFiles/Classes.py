e87ab2c83e05b139d53fd82a4828a7910844d45e
RHawkes <48405196+Hawkes301@users.noreply.github.com>
2026-01-27 12:32:45 +0000

    Ammending class inits

--- .venv/GitProjectFiles/Classes.py
+++ .venv/GitProjectFiles/Classes.py
@@ -1,48 +1,52 @@
 import datetime
 class Rehabilitation_Housing_Unit:
-    def __init__(self,HousingCategory,NighttimeCurfew,WeekendCurfew,Location,Licensees,SuitableYoungOffenders,MedicalServiceAccess,TransportLinks,CulturalReligeousServices,MentalHealthSuitable,Gender,FamilyAccess,Employment,StayPeriod,FutureExpansion1,FutureExpansion2,FutureExpansion3,StudentSuggested1,StudentSuggested2,CostPerBed,Capacity,EmergencyCapacity,ShortTermBeds,Affress,Phone,Email,Contact,ManagementGroup):
-        self.HousingCategory
+    def __init__(self,HousingCategory,NighttimeCurfew,WeekendCurfew,Location,Notes,Licensees,SuitableYoungOffenders,MedicalServiceAccess,TransportLinks,CulturalReligeousServices,MentalHealthSuitable,Gender,FamilyAccess,Employment,StayPeriod,FutureExpansion1,FutureExpansion2,FutureExpansion3,StudentSuggested1,StudentSuggested2,CostPerBed,Capacity,EmergencyCapacity,ShortTermBeds,Affress,Phone,Email,Contact,ManagementGroup):
+        self.HousingCategory = chr
         self.Location = Location
-        self.StayPeriod
-        self.CostPerBed
-        self.Capacity
-        self.EmergencyCapacity
-        self.ShortTermBeds
-        self.Address
-        self.Phone
-        self.Email
-        self.Contact
-        self.ManagementGroup
-        self.Gender
-        self.Licensees
-        self.SuitableYoungOffenders
-        self.NighttimeCurfew
-        self.WeekendCurfew
-        self.MedicalServiceAccess
-        self.TransportLinks
-        self.CulturalReligeousServices
-        self.MentalHealthSuitable
-        self.Employment
-        self.FamilyAccess
+        self.StayPeriod = int
+        self.CostPerBed = float
+        self.Capacity = int
+        self.EmergencyCapacity = int
+        self.ShortTermBeds = int
+        self.Address = str
+        self.Phone = str
+        self.Email = str
+        self.Contact = str
+        self.ManagementGroup = str
+        self.Gender = int
+        self.Licensees = list(Licensee)
+        self.SuitableYoungOffenders = bin
+        self.NighttimeCurfew = bin
+        self.WeekendCurfew = bin
+        self.MedicalServiceAccess = bin
+        self.TransportLinks = list(str)
+        self.CulturalReligeousServices = list(bin)
+        self.MentalHealthSuitable = list(bin)
+        self.Employment = list(bin)
+        self.FamilyAccess = bin
         self.FutureExpansion1
         self.FutureExpansion2
         self.FutureExpansion3
         self.StudentSuggested1
         self.StudentSuggested2
+        self.Notes = list(str)
 
 class Licensee:
-    def __init__(self,Name,HomeAddress,Gender,RoleID,ReleaseDate,ExpectedLicenceEnd,Notes,CurrentLocation,Category):
+    def __init__(self,Name,HomeAddress,Gender,RoleID,Disability,ReleaseDate,ExpectedLicenceEnd,Notes,CurrentLocation,Category):
         self.Name = str
         self.RoleID = str
         self.HomeAddress = str
-        self.Gender = str
+        self.Gender = int
         self.ReleaseDate = date()
         self.ExpectedLicenceEnd = date()
         self.Notes = list(str)
         self.CurrentLocation = str
         self.Category = chr
+        self.Disability = list(str)
+
 class Location:
-    def __init__(self,Name,SuitableForSexOffenders):
+    def __init__(self,Name,SuitableForSexOffenders,x,y):
         self.Name = str
         self.SuitableForSexOffenders = bin
-    pass
+        self.x = float
+        self.y = float
