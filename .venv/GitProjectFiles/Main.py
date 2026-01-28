11d6e0e6dccbc3adec983f1febfe96f62b022bbb
RHawkes <48405196+Hawkes301@users.noreply.github.com>
2026-01-28 19:22:26 +0000

    Adding main window pyside6

--- .venv/GitProjectFiles/Main.py
+++ .venv/GitProjectFiles/Main.py
@@ -1,5 +1,44 @@
 # reminder
 # “I am building a desktop PySide6 app
 # that helps one officer match people to housing.”
-class MainWindow():
-    pass
+from RHU import Rehabilitation_Housing_Unit
+from Licensee import Licensee
+from Allocation import OnLicenceHousingAllocationSystem, Location
+import sys
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        self.label = QLabel()
+
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
